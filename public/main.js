cc8899b40a577d4ac9b994ea0201c530894a19ec
axhindol <axhindol@umich.edu>
2017-03-16 11:40:07 -0400

    added console.log

--- public/main.js
+++ public/main.js
@@ -7,12 +7,12 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	if (!!window.EventSource) {
 		// Good example on using SSE
 		// http://www.html5rocks.com/en/tutorials/eventsource/basics/
-		
+
 		var source = new EventSource('data');
 		source.addEventListener('message', function(e) {
 			// e.data is the SSE data, which is a two-character hexadecimal string representing a value
-			// console.log("msg=" + e.data);
-                                
+			console.log("msg=" + e.data);
+
             for(var ii = 0; ii < e.data.length; ii += 2) {
                 handleData(parseInt('0x' + e.data.substr(ii, 2)));
             }
@@ -26,34 +26,34 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
 function handleData(data) {
 	// data is a number value (currently 0 - 255)
-	
+
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
-	
+
 	// console.log(data);
-	
+
 	// Add to the data set, remove from the left if it gets wider than the canvas
 	dataSet.push(data);
 	if (dataSet.length > (canvas.width - 1)) {
 		dataSet.shift();
 	}
-	
+
 	// Erase
 	ctx.fillStyle = "#ffffff";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);	
-	
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 	// Draw samples
 	ctx.fillStyle = "#000000";
-	
+
 	for(var ii = 0; ii < dataSet.length; ii++) {
 		// The canvas coordinate space increases going down the page, but the graph
-		// makes more sense flipped the other way so subtract the value from the 
+		// makes more sense flipped the other way so subtract the value from the
 		// maximum value
 		var yy = 255 - dataSet[ii];
 
 		ctx.fillRect(ii, yy, 1, 1);
 	}
-	
-	
+
+
 }
 
