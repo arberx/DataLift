ba65066dfb9e7787fd9961f18404f5f5d3b098f8
axhindol <axhindol@umich.edu>
2017-03-31 12:55:20 -0400

    .ccp added

--- firmware/livegraph.cpp
+++ firmware/livegraph.cpp
@@ -1,14 +1,23 @@
+#include <Adafruit_LIS3DH.h>
+#include <LIS3DH.h>
 #include "Particle.h"
+
+SYSTEM_THREAD(ENABLED);
 
 // How often to send samples in milliseconds
 const unsigned long SEND_PERIOD_MS = 20;
 
-// Analog input pin that's connected to a potentiometer in the test
-const int INPUT_PIN = A0;
+const unsigned long PRINT_SAMPLE_PERIOD = 200;
 
-// IP address and port of the server. Note that the node server uses two ports - one for the web browser
-// and a raw TCP port for receiving data. This is the port number for the data port, not the web server port!
-IPAddress serverAddress(192,168,2,4);
+LIS3DHSPI accel(SPI, A2, WKP);
+
+
+const int INPUT_PIN = A2;
+bool button_press = false;
+const int INPUT_PIN1 = D0;
+
+
+IPAddress serverAddress(138,197,27,109);
 int serverPort = 8081;
 
 // Finite state machine states
@@ -19,10 +28,26 @@ unsigned long lastSend = 0;
 int state = CONNECT_STATE;
 
 void setup() {
+    pinMode(INPUT_PIN1, INPUT_PULLUP);
+
 	Serial.begin(9600);
+    LIS3DHConfig config;
+    config.setAccelMode(LIS3DH::RATE_100_HZ);
+    bool setupSuccess = accel.setup(config);
+    Serial.printlnf("setupSuccess=%d", setupSuccess);
+
 }
 
 void loop() {
+    int button = digitalRead(INPUT_PIN1);
+    // if(button == HIGH && buttonpress == true){
+    //     buttonpress = false;
+    // }
+    if(button == LOW){
+        button_press = true;
+    }
+
+    if(button_press){
 	switch(state) {
 	case CONNECT_STATE:
 		Serial.println("connecting...");
@@ -47,9 +72,14 @@ void loop() {
 				lastSend = millis();
 
 				// analogRead returns 0 - 4095; remove the low bits so we got 0 - 255 instead.
-				int val = analogRead(INPUT_PIN) >> 4;
+				// int val = analogRead(INPUT_PIN) >> 4;
+				LIS3DHSample sample;
+				accel.getSample(sample);
+				int val = sample.z >> 4;
 
-				client.write((unsigned char)val);
+				Serial.println(val);
+
+				client.write(val);
 			}
 		}
 		else {
@@ -62,4 +92,5 @@ void loop() {
 		break;
 	}
 
+}
 }
